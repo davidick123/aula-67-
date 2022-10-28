c3119563ed5de7ce05a93304320c651666da7cd4
davidick123 <93011594+davidick123@users.noreply.github.com>
2022-10-28 15:34:02 -0300

    Add files via upload

---
+++ config(1).js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+// adicione SDK do Firebase
+var firebaseConfig = {
+    const firebaseConfig = {
+        apiKey: "AIzaSyC44qTmZ2yudIspVyQ_hUNesDF76l_3BzQ",
+        authDomain: "voto-27b8b.firebaseapp.com",
+        projectId: "voto-27b8b",
+        storageBucket: "voto-27b8b.appspot.com",
+        messagingSenderId: "986773780138",
+        appId: "1:986773780138:web:69141ea80469ca2dc242d3"
+      };
+};
+// Initialice o Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
